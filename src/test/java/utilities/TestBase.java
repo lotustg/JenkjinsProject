b44293843e77c71651ad8efcce8da49a67067317
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2022-11-12 15:32:40 +0300

    -

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,102 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public abstract class TestBase {
+
+    protected static WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+    }
+//    @After
+//    public void tearDown(){
+//        driver.quit();
+//    }
+
+    public void takeScreenShot() throws IOException {
+        //1. Take screenshot
+        File image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+        //2. Save Screenshot
+
+        String currentTime = new SimpleDateFormat("yyyyMMddhhmmssms").format(new Date());
+        String path = System.getProperty("user.dir")+"/test-output/screenshots/"+currentTime+"image.png";
+
+        FileUtils.copyFile(image,new File(path));
+
+    }
+
+
+
+//    creating a resuable method to scroll into view of an element
+    public void scrollIntoViewJS(WebElement element){
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",element);
+
+    }
+    //    Scroll all the way down of a page
+    public void scrollAllDownByJS(){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+    }
+    //    Scroll al the way up of a page
+    public void scrollAllUpByJS(){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("window.scrollTo(0,-document.body.scrollHeight)");
+    }
+    //    Click on a specific element. Param: WebElement
+    public void clickByJS(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].click()",element);
+    }
+
+//    Set the value of an input using js executor. Params: WebElement element, String text
+//    This method changes the value attribute of an element.
+//    It changes the input text. NOT COMMON
+    public void setValueByJS(WebElement element, String text){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].setAttribute('value','"+text+"')",element);
+    }
+
+    //    get the value of an input. param: idOfElement
+    public void getValueByJS(String idOfElement){
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        String value=js.executeScript("return document.getElementById('"+idOfElement+"').value").toString();
+        System.out.println(value);
+//        How you get the value of an input box?
+//        We can use js executor.
+//        How?
+//        I can get the element using js executor, and get the value of the element.
+//        For example, I can get the element by id, and use value attribute to get the value of in an input
+//        I have to do this, cause getText in this case does not return teh text in an input
+    }
+
+    //    Changes the changeBackgroundColorByJS of an element. Params: WebElement element, String color. NOT COMMON
+    public void changeBackgroundColorByJS(WebElement element, String color){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].style.backgroundColor='"+color+"'",element);
+    }
+
+    //    NOT COMMON
+    public void addBorderWithJS(WebElement element, String borderStyle){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].style.border='"+borderStyle+"'",element);
+    }
+}
